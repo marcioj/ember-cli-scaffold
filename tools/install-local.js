600772dee630a89cfd3c33a9baaee74a43c69955
Marcio Junior <marciojunior_eu@yahoo.com.br>
2016-06-25 14:44:43 -0300

    Add install-local.js script to test this addon in some ember project

---
+++ tools/install-local.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+// Description: like ember install addon-name but linking from this source code
+// Usage: call ./install-local.js from within a ember-cli project
+
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').execSync;
+
+function sh(command) {
+  return exec(command, { encoding: 'utf8', stdio: ['pipe', 'pipe', 'inherit'] });
+}
+
+function info(msg) {
+  console.log(msg);
+}
+
+function readJson(path) {
+  return JSON.parse(fs.readFileSync(path));
+}
+
+function writeJson(path, obj) {
+  fs.writeFileSync(path, JSON.stringify(obj));
+}
+
+var addonName = path.dirname(path.relative('..',  __dirname));
+var json = readJson('./package.json');
+
+info(`Linking ${addonName}`);
+sh(`npm link ${addonName}`);
+
+info(`Including ${addonName} dependency into package.json`);
+json.devDependencies[addonName] = '*';
+writeJson('./package.json', json);
+
+info(`Running default addon command`);
+sh(`ember generate ${addonName}`);
